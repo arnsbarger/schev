5b54cccfb5fb77dd6674fe9804987acb3b538af5
Madison Arnsbarger <maddieka@vt.edu>
2017-09-06 14:57:48 -0400

    list error for dan

--- cleaning_schev_data.R
+++ cleaning_schev_data.R
@@ -14,18 +14,22 @@ i <- 1
 college <- schev[i]
 
 # j of 133 counties
-j <- 1
-county <- college[[j]] 
-
-final_product[(9*i-8):(9*i),"college_fips"] <- names(county[j])
-
-# number of applications - store in final_product DataTable_apps231420.Count
-college_fips <- as.data.frame(county[j])
-final_product[(9*i-8):(9*i),"fall_term"] <- college_fips[1:9,2]
-#final_product[(9*i-8):(9*i),"num_applications"] <- college_fips[1:9,grep("apps", colnames(college_fips))] # apps Count
-final_product[(9*i-8):(9*i),"num_applications"] <- college_fips[1:9,3]
-final_product[(9*i-8):(9*i),"num_admitted"] <- college_fips[1:9,6]
-final_product[(9*i-8):(9*i),"num_enrolled"] <- college_fips[1:9,9]
-final_product[(9*i-8):(9*i),"admit_rate"] <- college_fips[1:9,12]
-final_product[(9*i-8):(9*i),"yield_rate"] <- college_fips[1:9,15]
+j <- 2
+for (j in 1:133){
+    county <- college[[j]]
+    
+    final_product[(9*i-8):(9*i),"college_fips"] <- names(county[j])
+    
+    # number of applications - store in final_product DataTable_apps231420.Count
+    college_fips <- as.data.frame(county[j])
+    final_product[(9*i-8):(9*i),"fall_term"] <- college_fips[1:9,2]
+    #final_product[(9*i-8):(9*i),"num_applications"] <- college_fips[1:9,grep("apps", colnames(college_fips))] # apps Count
+    final_product[(9*i-8):(9*i),"num_applications"] <- college_fips[1:9,3]
+    final_product[(9*i-8):(9*i),"num_admitted"] <- college_fips[1:9,6]
+    final_product[(9*i-8):(9*i),"num_enrolled"] <- college_fips[1:9,9]
+    final_product[(9*i-8):(9*i),"admit_rate"] <- college_fips[1:9,12]
+    final_product[(9*i-8):(9*i),"yield_rate"] <- college_fips[1:9,15]
+    
+    print(j)
+}
 
