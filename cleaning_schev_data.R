3b686cbea2f883e3eddcc810e29fec5e247191b3
Madison Arnsbarger <maddieka@vt.edu>
2017-09-12 09:08:16 -0400

    end of 1st attempt. begin 2nd attempt. 2nd attempt WORKS

--- cleaning_schev_data.R
+++ cleaning_schev_data.R
@@ -8,9 +8,11 @@ final_product <- data.frame(college_fips = character(),
                             admit_rate = numeric(),
                             yield_rate = numeric(),
                             stringsAsFactors = FALSE)
+# remove empty data frames from schev
+dim(schev$`Blue Ridge Community College`$`Blue Ridge Community College_013`$DataTable_yield231536)
 
 # i in 79 colleges
-for (i in 1:79){
+for (i in 1:3){ # 79
     college <- schev[i]
     
     # j of 133 counties
@@ -19,22 +21,26 @@ for (i in 1:79){
         
         college_fips <- as.data.frame(county[(j*i)])
         
-        # a usually = 9 (and b = 8) because there are 9 years recorded (2007-2016)
-        # some empty data frames (no data collected for any years) cause errors
-        # therefore we make a, b dependent on the inputs
-        a <- as.numeric(nrow(as.data.frame(county[(j*i)]))) 
-        b <- (a - 1)
+        if (nrow(college_fips) > 0){
+            
+            # a usually = 9 (and b = 8) because there are 9 years recorded (2007-2016)
+            # some empty data frames (no data collected for any years) cause errors
+            # therefore we make a, b dependent on the inputs
+            a <- as.numeric(nrow(as.data.frame(county[(j*i)]))) 
+            b <- (a - 1)
+            
+            final_product[(a*(j*i)-b):(a*(j*i)),"college_fips"] <- names(county[(j*i)])
+            
+            final_product[(a*(j*i)-b):(a*(j*i)),"fall_term"] <- college_fips[1:a,2]
+            final_product[(a*(j*i)-b):(a*(j*i)),"num_applications"] <- college_fips[1:a,3]
+            final_product[(a*(j*i)-b):(a*(j*i)),"num_admitted"] <- college_fips[1:a,6]
+            final_product[(a*(j*i)-b):(a*(j*i)),"num_enrolled"] <- college_fips[1:a,9]
+            final_product[(a*(j*i)-b):(a*(j*i)),"admit_rate"] <- college_fips[1:a,12]
+            final_product[(a*(j*i)-b):(a*(j*i)),"yield_rate"] <- college_fips[1:a,15]
+            
+            print(j)
+        }
         
-        final_product[(a*(j*i)-b):(a*(j*i)),"college_fips"] <- names(county[(j*i)])
-        
-        final_product[(a*(j*i)-b):(a*(j*i)),"fall_term"] <- college_fips[1:a,2]
-        final_product[(a*(j*i)-b):(a*(j*i)),"num_applications"] <- college_fips[1:a,3]
-        final_product[(a*(j*i)-b):(a*(j*i)),"num_admitted"] <- college_fips[1:a,6]
-        final_product[(a*(j*i)-b):(a*(j*i)),"num_enrolled"] <- college_fips[1:a,9]
-        final_product[(a*(j*i)-b):(a*(j*i)),"admit_rate"] <- college_fips[1:a,12]
-        final_product[(a*(j*i)-b):(a*(j*i)),"yield_rate"] <- college_fips[1:a,15]
-        
-        print(j)
     }
     
     print(i)
---
+++ cleaning_schev_data2.R
@@ -0,0 +1,52 @@
+schev <- readRDS("Documents/SDAL/SCHEV/data/schev_col_cos.RDS") # list - 79 colleges
+
+final_product <- data.frame(college_fips = character(),
+                            fall_term = numeric(),
+                            num_applications = numeric(),
+                            num_admitted = numeric(),
+                            num_enrolled = numeric(),
+                            admit_rate = numeric(),
+                            yield_rate = numeric(),
+                            stringsAsFactors = FALSE)
+
+# i in 79 colleges
+for (i in 1:79){ 
+    college <- schev[i]
+    
+    # j of 133 counties
+    for (j in 1:133){
+        county <- college[[1]]
+        
+        college_fips <- as.data.frame(county[(j*i)])
+        
+        # some colleges contain lists of 5 empty data frames.
+        # we exclude these because they appear as rows of na's in the final product anyways.
+        if (nrow(college_fips) > 0){ 
+            
+            # a usually = 9 (and b usually = 8) because there are 9 years recorded (2007-2016).
+            # some empty data frames (no data collected for any years) cause errors.
+            # therefore we make a, b (the dimensions of the df we rbind to final product) dependent on the inputs.
+            a <- as.numeric(nrow(as.data.frame(county[(j*i)]))) 
+            b <- (a - 1)
+            
+            school_df <- data.frame(college_fips = names(county[(j*i)]),
+                                    fall_term = college_fips[1:a,2],
+                                    num_applications = college_fips[1:a,3],
+                                    num_admitted = college_fips[1:a,6],
+                                    num_enrolled = college_fips[1:a,9],
+                                    admit_rate = college_fips[1:a,12],
+                                    yield_rate = college_fips[1:a,15])
+            
+            final_product <- rbind(final_product, school_df)
+            
+            print(j)
+        }
+        
+    }
+    
+    print(i)
+}
+
+
+
+
