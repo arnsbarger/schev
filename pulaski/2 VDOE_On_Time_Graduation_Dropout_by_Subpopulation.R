9e20cac0f6a82ebc33fa41a12110b915dbf9f46a
Madison Arnsbarger <maddieka@vt.edu>
2018-01-08 11:04:44 -0500

    recreating schev data for NRV

---
+++ pulaski/2 VDOE_On_Time_Graduation_Dropout_by_Subpopulation.R
@@ -0,0 +1,125 @@
+library(dplyr)
+library(stringr)
+library(reshape2)
+library(gdata)
+
+## get Virginia school data
+setwd("~/Google Drive/SDAL Google Drive Folders/SCHEV (Peter Blake - Wendy Kang)/Data/VDOE/On-Time Graduation Rate and Cohort Dropout Rate/")
+
+## school crosswalk
+sch_cw<-read.csv("~/Google Drive/SDAL Google Drive Folders/SCHEV (Peter Blake - Wendy Kang)/Code/Bianica/school_crosswalk.csv",stringsAsFactors = F)
+
+# postsec enrollment - 2011-2013
+filenames<-list.files(pattern="_cohort.csv", full.names=TRUE)
+
+for (i in 1: length(filenames)) {
+  
+  tmp<-read.csv(filenames[i],stringsAsFactors = F)
+  
+  if (i==1) {
+    data1<-tmp
+  } else {
+    data1<-rbind(data1,tmp)
+  }
+}
+
+data1$DIV_NAME <- str_trim(data1$DIV_NAME)
+data1$SCH_NAME <- str_trim(data1$SCH_NAME)
+
+write.csv(data1,"~/Google Drive/SDAL Google Drive Folders/SCHEV (Peter Blake - Wendy Kang)/Code/Bianica/vdoe_on_time_grad_all_schools.csv")
+
+# get postsec enrollment by subpopulations and by 2-year and 4-year
+grad<-filter(data1, DIV_NAME=="Sussex County" | DIV_NAME=="Powhatan County" | DIV_NAME=="Richmond City" | DIV_NAME=="Buchanan County" |
+                   DIV_NAME=="Bland County" | DIV_NAME=="Roanoke County" | DIV_NAME=="Roanoke City")
+
+grad<-filter(grad,DIV_NAME!="" & SCH_NAME=="")
+
+# get clean school names
+grad$SCH_NAME2<-toupper(grad$SCH_NAME)
+grad<-left_join(grad,sch_cw,by=c("SCH_NAME2"="sch_names"),ignore.case=TRUE)
+
+# sum using clean school name
+grad1<-group_by(grad,SCHOOL_YEAR,div_num,div_name,sch_name_clean,GENDER,LEP_FLAG,
+                    DISADVANTAGED_FLAG,DISABILITY_FLAG,FEDERAL_RACE_CODE) %>%
+  summarise(cohort_graduate_cnt=sum(COHORT_CNT),diploma_rate=mean(DIPLOMA_RATE),dropout_rate=mean(DROPOUT_RATE))
+
+# convert ontime grade rate and dropout rate to counts
+grad1$ontime_grad_cnt<-as.integer(grad1$cohort_graduate_cnt*(grad1$diploma_rate/100))
+grad1$cohort_dropout_cnt<-as.integer(grad1$cohort_graduate_cnt*(grad1$dropout_rate/100))
+
+## get total on time grad and dropout across all subpopulations
+gradAll<-filter(grad1, GENDER=="" & DISABILITY_FLAG=="" & LEP_FLAG=="" & DISADVANTAGED_FLAG=="" 
+              & is.na(FEDERAL_RACE_CODE))
+gradAll<-group_by(gradAll,SCHOOL_YEAR,div_num,div_name,sch_name_clean) %>%
+  summarise(onTimeTotal=sum(ontime_grad_cnt),dropoutTotal=sum(cohort_dropout_cnt))
+
+########## DISADVANTAGED ###############
+# get on time graduation and dropout by disadvantaged
+gradDisadv <- grad1 %>%
+  filter(GENDER=="" & DISABILITY_FLAG=="" & LEP_FLAG=="" & DISADVANTAGED_FLAG!=""
+         & is.na(FEDERAL_RACE_CODE))
+
+# add total
+gradDisadv<-left_join(gradDisadv,gradAll,by=c("SCHOOL_YEAR","div_num","div_name","sch_name_clean"))
+
+## check for drop offs
+checkDisadv1<-group_by(gradDisadv,SCHOOL_YEAR,div_num,div_name,sch_name_clean,onTimeTotal,dropoutTotal) %>%
+  summarise(grad_cnt=sum(ontime_grad_cnt),do_cnt=sum(cohort_dropout_cnt))
+checkDisadv1$diff<-checkDisadv1$onTimeTotal-checkDisadv1$grad_cnt
+sum(checkDisadv1$diff)/sum(checkDisadv1$onTimeTotal)
+#[1] 0.003318434
+checkDisadv1$diff<-checkDisadv1$dropoutTotal-checkDisadv1$do_cnt
+sum(checkDisadv1$diff)/sum(checkDisadv1$dropoutTotal)
+#[1] 0.02402957
+
+# sum using clean school name
+gradDisadv<-gradDisadv[,c(1:4,7,10,13:16)]
+write.csv(gradDisadv,"~/Google Drive/SDAL Google Drive Folders/SCHEV (Peter Blake - Wendy Kang)/Code/Bianica/vdoe_ontimegrad_dropout_by_disadv.csv")
+
+########## RACE/ETHNICITY ###############
+# get on time graduation and dropout by disadvantaged
+gradRace <- grad1 %>%
+  filter(GENDER=="" & DISABILITY_FLAG=="" & LEP_FLAG=="" & DISADVANTAGED_FLAG==""
+         & !is.na(FEDERAL_RACE_CODE))
+
+# add total
+gradRace<-left_join(gradRace,gradAll,by=c("SCHOOL_YEAR","div_num","div_name","sch_name_clean"))
+
+## check for drop offs
+check1<-group_by(gradRace,SCHOOL_YEAR,div_num,div_name,sch_name_clean,onTimeTotal,dropoutTotal) %>%
+  summarise(grad_cnt=sum(ontime_grad_cnt),do_cnt=sum(cohort_dropout_cnt))
+check1$diff<-check1$onTimeTotal-check1$grad_cnt
+sum(check1$diff)/sum(check1$onTimeTotal)
+#[1] 0.03223622
+check1$diff<-check1$dropoutTotal-check1$do_cnt
+sum(check1$diff)/sum(check1$dropoutTotal)
+#[1] 0.05113986
+
+# sum using clean school name
+gradRace<-gradRace[,c(1:4,9,10,13:16)]
+write.csv(gradRace,"~/Google Drive/SDAL Google Drive Folders/SCHEV (Peter Blake - Wendy Kang)/Code/Bianica/vdoe_ontimegrad_dropout_by_race.csv")
+
+########## GENDER ###############
+# get on time graduation and dropout by disadvantaged
+gradGender <- grad1 %>%
+  filter(GENDER!="" & DISABILITY_FLAG=="" & LEP_FLAG=="" & DISADVANTAGED_FLAG==""
+         & is.na(FEDERAL_RACE_CODE))
+
+# add total
+gradGender<-left_join(gradGender,gradAll,by=c("SCHOOL_YEAR","div_num","div_name","sch_name_clean"))
+
+## check for drop offs
+check1<-group_by(gradGender,SCHOOL_YEAR,div_num,div_name,sch_name_clean,onTimeTotal,dropoutTotal) %>%
+  summarise(grad_cnt=sum(ontime_grad_cnt),do_cnt=sum(cohort_dropout_cnt))
+check1$diff<-check1$onTimeTotal-check1$grad_cnt
+sum(check1$diff)/sum(check1$onTimeTotal)
+#[1] 0.002844372
+check1$diff<-check1$dropoutTotal-check1$do_cnt
+sum(check1$diff)/sum(check1$dropoutTotal)
+#[1] 0.01478743
+
+# sum using clean school name
+gradGender<-gradGender[,c(1:5,10,13:16)]
+write.csv(gradGender,"~/Google Drive/SDAL Google Drive Folders/SCHEV (Peter Blake - Wendy Kang)/Code/Bianica/vdoe_ontimegrad_dropout_by_gender.csv")
+
+### UPDATE DATA ON MORE RECENT YEARS FROM SCHOOL QUALITY REPORTS
