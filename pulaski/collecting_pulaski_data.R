6632ae7227a3eb59df29167256409f8108ef1606
Madison Arnsbarger <maddieka@vt.edu>
2018-01-10 14:00:53 -0500

    added clean school names column for future merges

--- pulaski/collecting_pulaski_data.R
+++ pulaski/collecting_pulaski_data.R
@@ -34,8 +34,16 @@ vdoe_hs_completer_by_disadv_allVA$year_fall <- as.numeric(gsub("\\-.*","", vdoe_
 vdoe_hs_completer_by_gender_allVA <- read_csv("~/Google Drive/SCHEV (Peter Blake - Wendy Kang)/Code/Maddie/pulaski/vdoe_hs_completer_dimploma_by_gender-allVA.csv")
 vdoe_hs_completer_by_gender_allVA$year_fall <- as.numeric(gsub("\\-.*","", vdoe_hs_completer_by_gender_allVA$SCHOOL_YEAR))
 
+# SCHOOL SUMMARY
+vdoe_hs_total_grade_race <- read_csv("Code/Bianica/vdoe_hs_total_grade_race.csv")
+
+
 # CTE ####
 cte2014 <- read_csv("~/Google Drive/SCHEV (Peter Blake - Wendy Kang)/Data/VDOE/Career & Technical Education Program Graduates and Completers/2014-cte-comp.csv")
+
+# STUDENT COUNTS ####
+student_counts_allVA <- read.csv("~/Google Drive/SCHEV (Peter Blake - Wendy Kang)/Code/Maddie/pulaski/2011-2016student_counts-allVA.csv")
+student_counts_allVA$sch_name_clean <- str_to_title(student_counts_allVA$SCH_NAME)
 
 save(list = ls(all.names = TRUE), file = "Code/Maddie/pulaski/pulaski_vis_data.RData", envir = .GlobalEnv)
 
